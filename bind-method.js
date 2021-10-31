f386988cdb0c8b5e401a4dab34b364eeeba5aa14
arifulhaqshahed <arifulhaqueshahed21@gmail.com>
2021-10-31 21:45:02 +0600

    bind to borrow method from an array to another array

---
+++ bind-method.js
@@ -0,0 +1,18 @@
+const kibria = {
+    name: 'RJ Kibria',
+    id: 101,
+    money: 10000,
+    givenTreat: function(expense){
+        this.money = this.money - expense;
+        return this.money;
+    }
+}
+
+const balam = {
+    name: 'singer Balam',
+    id: 102,
+    money: 5000
+}
+const treatwillGiven = kibria.givenTreat.bind(balam);
+treatwillGiven(500);
+console.log(balam);
